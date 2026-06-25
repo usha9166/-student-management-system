262af9bf027b6d477d1e996410d9a9270168b169
Usha Agarwal <usharoyal23@gmail.com>
2026-06-25 12:58:01 +0530

    fix axios baseURL

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: '/api/v1',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:4000/api/v1',
   withCredentials: true,
   headers: { 'Content-Type': 'application/json' },
 });
