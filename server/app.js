95f97a474f11c5dc26adae2a60b3a3d1c02a850a
Usha Agarwal <usharoyal23@gmail.com>
2026-06-25 15:05:22 +0530

    fix CORS multiple origins

--- server/app.js
+++ server/app.js
@@ -17,7 +17,11 @@ const __dirname = path.dirname(__filename);
 const app = express();
 
 app.use(cors({
-  origin: [process.env.FRONTED_URL],
+  origin: [
+    process.env.FRONTED_URL,
+    "https://student-management-system-khaki-iota.vercel.app",
+    "https://student-management-system-pzwtg7ybh-ushas-projects-52063d81.vercel.app"
+  ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }));
